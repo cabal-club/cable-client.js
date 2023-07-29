fe70e689d3b1e9d4649e8ac924c5b301ec731d96
cblgh <cblgh@cblgh.org>
2023-07-29 11:19:54 +0200

    implement a few missing bits and categorize methods according to type

--- mock.js
+++ mock.js
@@ -1,3 +1,5 @@
+/* translation layer between old cabal-client api and the new library cable-client. 
+ * this allows cabal-cli to operate on `cable-client` without needing to change a cabal-cli instance :) */
 const EventEmitter = require("events").EventEmitter
 
 class User {
@@ -9,6 +11,7 @@ class User {
   isModerator() { return false }
   isHidden() { return false }
 }
+
 class CabalDetails extends EventEmitter {
   constructor() {
     super()
@@ -16,6 +19,7 @@ class CabalDetails extends EventEmitter {
     this.statusMessages = []
     this.chat = {"default": []}
     this.localUser = new User("123412341234", "mock-user")
+    this.showIds = false
     this.channels = ["default"]
     this.chindex = 0
     this.topics = {"default": "placeholder topic"}
@@ -25,7 +29,7 @@ class CabalDetails extends EventEmitter {
   getChat() {
     return this.chat[this.getCurrentChannel()]
   }
-  getUsers() { return [] }
+
   getTopic() { return this.topics[this.getCurrentChannel()] }
   focusChannel(ch) {
     this.chindex = this.channels.indexOf(ch)
@@ -35,8 +39,26 @@ class CabalDetails extends EventEmitter {
   getChannels() { return this.channels }
   getCurrentChannel() { return this.channels[this.chindex] }
   isChannelPrivate(ch) { return false }
+  getUsers() { 
+    const key = this.localUser.key
+    const users = {}
+    users[key] = this.localUser
+    return users
+  }
   getChannelMembers() { return [this.localUser] }
-  addStatusMessage(m) { this.statusMessages.push(m) }
+  addStatusMessage(m) { 
+    console.log(m);this.statusMessages.push(m) 
+    this.chat[this.getCurrentChannel()].push({ 
+      key: this.localUser.key, 
+      value: { 
+        timestamp: +(new Date()),
+        type: "status",
+        content: {
+          text: m.text
+        }
+      }
+    })
+  }
   processLine(line) {
     if (line.startsWith("/")) {
       const delim = line.indexOf(" ")
@@ -73,7 +95,6 @@ class CabalDetails extends EventEmitter {
     }, 40)
   }
   publishMessage() { }
-  showIds() { this.chat.push("show ids") }
 }
 
 class Client {
@@ -82,24 +103,33 @@ class Client {
     this.cabals = []
     this.cabalKeys = []
   }
-  createCabal() { return this.details }
-  getCommands() {}
-  getUsers() { return [this.details.localUser] }
   getJoinedChannels() { return ["default"] }
-  getNumberUnreadMessages() { return 0 }
-  getMentions() { return [] }
-  getDetails (key) { return this.details }
+
+  /* methods where we punt to cabal details */
+  getUsers() { return this.details.getUsers() }
   getMessages(opts, cb) { cb(this.details.getChat()) }
   focusChannel(ch) { this.details.focusChannel(ch) }
+
   static getDatabaseVersion () { return "v1.3.37" }
   static getCabalDirectory() { return "/home/cblgh/code/cabal-club/grant-work-2022/cable-client/cabal-test" }
-  markChannelRead(ch) { }
-  getCurrentCabal() { return this.details }
+
+  /* variations of the same: getting the cabal instance */
+  // for cable-client, we'll only operate on a single cabal 
   cabalToDetails () { return this.getCurrentCabal() }
+  createCabal() { return this.details }
+  getDetails (key) { return this.details }
+  getCurrentCabal() { return this.details }
   focusCabal() { return this.details }
+  _getCabalByKey(key) { return this.details }
+
   getCabalKeys() { return [this.details.key] }
+
+  /* unimplemented/touched methods */
+  getNumberUnreadMessages() { return 0 }
+  getMentions() { return [] }
+  markChannelRead(ch) { }
   addCabal(key) {}
-  _getCabalByKey(key) {return this.details}
+  getCommands() {}
 }
 
 module.exports = {
